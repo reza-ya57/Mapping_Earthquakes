00c4aa8604ec844a403781470607af2ea7ea0399
Reza Ya <r.yazdian.a@gmail.com>
2021-06-03 23:40:13 -0400

    add multiple market and popup marker

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -7,13 +7,35 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      34.0522, -118.2437
+      44.0522, -88.2437
     ],
-    zoom: 14
+    zoom: 4
   });
 
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city){
+//     console.log(city)
+//     L.marker(city.location)
+//     .bindPopup("<h2>"+ city.city + ", "+ city.state+ "</h2><hr><h3>Population "+ city.population.toLocaleString()+"</h3>")
+//     .addTo(map);
+// });
+
+cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        lineWeight: 0
+    })       
+    .bindPopup("<h2>"+ city.city + ", "+ city.state+ "</h2><hr><h3>Population "+ city.population.toLocaleString()+"</h3>")
+    .addTo(map);
+});
+
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // L.circle([34.0522, -118.2437], {
 //   color: 'black',
@@ -21,11 +43,11 @@ let marker = L.marker([34.0522, -118.2437]).addTo(map);
 //   radius: 300
 // }).addTo(map);
 
-L.circleMarker([34.0522, -118.2437],{
-  radius: 200,
-  color: "black",
-  fillColor: "#ffffa1"
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437],{
+//   radius: 200,
+//   color: "black",
+//   fillColor: "#ffffa1"
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
