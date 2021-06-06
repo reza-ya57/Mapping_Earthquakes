af74a625e606ccde85521ae3da926ab9a6a43dab
Reza Ya <r.yazdian.a@gmail.com>
2021-06-05 23:09:27 -0400

    Add multiple map by using control layer function

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,56 @@
 console.log("working");
 
 // Create the map object with a center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+//let map = L.map('mapid').setView([30, 30], 2);
+
+
+//Create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+//Add tile to the Map
+//street.addTo(map);
+
+// Create a base layer that holds both maps.
+let baseMaps ={
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30,30],
+  zoom:2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+//Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/reza-ya57/Mapping_Earthquakes/main/majorAirports.json"
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data){
+  console.log(data);
+  //Creating a GeoJson layer with the retrieved data.
+  //L.geoJson(data).addTo(map);
+   L.geoJSON(data, {
+     onEachFeature: function(feature, layer){
+       layer.bindPopup("<h2>Airport Code: "+feature.properties.faa+"</h2><hr><h3>Airport Name: "+feature.properties.name+"</h3>")
+     }
+   }).addTo(map);
+  })
+
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -34,25 +83,29 @@ let sanFranAirport =
 //   }
 // }).addTo(map);
 
-// Grabbing our GeoJSON data.Using onEachFeature Function
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>Airport Code: "+ feature.properties.faa +"</h2><hr><h3>Airport Name: "+feature.properties.name+"</h3>");
+
+// // Grabbing our GeoJSON data.Using onEachFeature Function
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2>Airport Code: "+ feature.properties.faa +"</h2><hr><h3>Airport Name: "+feature.properties.name+"</h3>");
     
-  }
-}).addTo(map);
+//   }
+// }).addTo(map);
 
-//Create tile layer
-let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  accessToken: API_KEY
-});
+// //Create tile layer
+// let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//   maxZoom: 18,
+//   accessToken: API_KEY
+// });
 
-//Add tile to Map
-street.addTo(map);
+
+
+
+// //Add tile to Map
+// street.addTo(map);
 
 
 
