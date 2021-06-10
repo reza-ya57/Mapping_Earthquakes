38c2036926c40a051623272112c35e266593c981
Reza Ya <r.yazdian.a@gmail.com>
2021-06-09 23:48:28 -0400

    Remove extra comment line to clean up the code

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -15,6 +15,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
+// Create the third tile layer that will be the background of our map. 
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
